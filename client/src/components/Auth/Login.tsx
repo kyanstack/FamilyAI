77aa4d1aa9d8045489ee952078467f739b153f6d
Hanlin-Zhou <zhou@example.com>
2023-06-26 17:54:46 +0800

    follow up on .DS_store

--- client/src/components/Auth/Login.tsx
+++ client/src/components/Auth/Login.tsx
@@ -32,7 +32,9 @@ function Login() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-white pt-6 sm:pt-0">
       <div className="mt-6 w-96 overflow-hidden bg-white px-6 py-4 sm:max-w-md sm:rounded-lg">
-        <h1 className="mb-4 text-center text-3xl font-semibold">{t('greeting')}</h1>
+        <h1 className="mb-4 text-center text-3xl font-semibold">
+          {t('greeting')}
+          </h1>
         {error && (
           <div
             className="relative mt-4 rounded border border-red-400 bg-red-100 px-4 py-3 text-red-700"
